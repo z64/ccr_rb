81249ee935000787e31d7cbbb0b1554513c6049a
z64 <zachnowicki@gmail.com>
2017-01-09 03:52:41 -0500

    enhance eval to use custom embed

--- src/modules/commands/eval.rb
+++ src/modules/commands/eval.rb
@@ -9,8 +9,15 @@ module Bot
         break unless event.user.id == CONFIG.owner
         begin
           eval code.join(' ')
-        rescue => e
-          "An error occurred 😞 ```#{e}```"
+        rescue => error
+          event.channel.send_embed do |e|
+            e.color = 0xff0000
+            e.description = "```#{error}```"
+            e.author = {
+              name: 'An error occurred!',
+              icon_url: 'http://emojipedia-us.s3.amazonaws.com/cache/f4/63/f463408675b9437b457915713b9af46c.png'
+            }
+          end
         end
       end
     end
