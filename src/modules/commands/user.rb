bb9e5e2d6c65c31e374a14f41e36aef932dc2fd8
z64 <zachnowicki@gmail.com>
2017-01-10 04:04:12 -0500

    add :user

---
+++ src/modules/commands/user.rb
@@ -0,0 +1,71 @@
+require 'upmark'
+
+module Bot
+  module DiscordCommands
+    module User
+      extend Discordrb::Commands::CommandContainer
+
+      command(:user,
+              description: 'shows you information about a user',
+              usage: 'ccr username',
+              min_args: 1) do |event, *name|
+        name = name.join ' '
+        user = OSU.user name
+        next 'user not found' unless user
+        event.channel.send_embed(
+          "`user info:` **#{user.name}**",
+          user_embed(user)
+        )
+      end
+
+      module_function
+
+      def user_embed(user)
+        e = Discordrb::Webhooks::Embed.new(
+          title: '[View Profile]',
+          url: user.profile_url,
+          footer: { text: "osu-api v#{Osu::VERSION}" },
+          timestamp: Time.now,
+          color: 0xFF69B4
+        )
+
+        e.add_field(
+          name: 'Ranks',
+          inline: true,
+          value: <<~data
+            **PP Rank:** #{user.pp_rank} (`#{user.pp_raw.round(2)}`) / **Country (#{user.country}):** #{user.pp_country_rank}
+            **SS** `x#{user.count_rank[:ss]}` / **S** `x#{user.count_rank[:s]}` / **A** `x#{user.count_rank[:a]}`
+            **Level #{user.level.round(2)}**
+          data
+        )
+
+        e.add_field(
+          name: 'Hits',
+          inline: true,
+          value: <<~data
+            **Accuracy:** `#{user.accuracy.round(2)}%`
+            300 `x#{user.count300}` / 100 `x#{user.count100}` / 50 `x#{user.count50}`
+          data
+        )
+
+        e.add_field(
+          name: 'Score',
+          inline: true,
+          value: <<~data
+            **Total:** #{user.total_score}
+            **Ranked:** #{user.ranked_score}
+          data
+        )
+
+        if user.events.any?
+          e.add_field(
+            name: 'Events',
+            value: user.events.map { |ev| Upmark.convert ev.display_html }.join("\n")
+          )
+        end
+
+        e
+      end
+    end
+  end
+end
