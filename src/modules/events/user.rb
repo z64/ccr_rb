bf51a3973b4dfed497b819231ea5aedee1ec13c8
z64 <zachnowicki@gmail.com>
2017-01-16 05:14:15 -0500

    add a shortcut event for ccr user

---
+++ src/modules/events/user.rb
@@ -0,0 +1,17 @@
+module Bot
+  module DiscordEvents
+    module User
+      extend Discordrb::EventContainer
+
+      message(start_with: 'ccr ') do |event|
+        name = event.message.content[4..-1]
+        puts name
+        BOT.execute_command(
+          :user,
+          Discordrb::Commands::CommandEvent.new(event.message, BOT),
+          name
+        )
+      end
+    end
+  end
+end
