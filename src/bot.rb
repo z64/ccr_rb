dbcb402a0871b6fa25328c5e6754a85a5ad5c360
z64 <zachnowicki@gmail.com>
2017-01-16 05:53:13 -0500

    disable library help command

--- src/bot.rb
+++ src/bot.rb
@@ -20,7 +20,8 @@ module Bot
   # can access the cache anywhere.
   BOT = Discordrb::Commands::CommandBot.new(client_id: CONFIG.client_id,
                                             token: CONFIG.token,
-                                            prefix: CONFIG.prefix)
+                                            prefix: CONFIG.prefix,
+                                            help_command: false)
 
   # Discord commands
   module DiscordCommands; end
