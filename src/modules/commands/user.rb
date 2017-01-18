fcadb7b2977c3babd130dcb7c7d7f7fb46550f58
z64 <zachnowicki@gmail.com>
2017-01-18 00:17:41 -0500

    expose ui to specify game mode in :user

--- src/modules/commands/user.rb
+++ src/modules/commands/user.rb
@@ -6,9 +6,11 @@ module Bot
       command(:user,
               description: 'shows you information about a user',
               usage: 'ccr username',
-              min_args: 1) do |event, *name|
+              min_args: 1) do |event, maybe_mode, *name|
         name = name.join ' '
-        user = OSU.user name
+        mode = maybe_mode.to_sym
+        user   = OSU.user name, mode.to_sym if Osu::API::MODE.include? mode
+        user ||= OSU.user maybe_mode + name
         next '`user not found`' unless user
         event.channel.send_embed(
           "`user info:` **#{user.name}**",
