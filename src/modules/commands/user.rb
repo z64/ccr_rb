8e812e71842b47549708fe0f5e8a2b616baa8ba1
z64 <zachnowicki@gmail.com>
2017-01-25 01:58:43 -0500

    add a command that displays recent events in the past week, closes #2

--- src/modules/commands/user.rb
+++ src/modules/commands/user.rb
@@ -17,6 +17,35 @@ module Bot
         )
       end
 
+      command(:events,
+              description: 'shows you recent events on a user\'s profile',
+              usage: 'ccr.events username',
+              min_args: 1) do |event, *name|
+        name = name.join ' '
+        user = OSU.user name, event_days: 7
+        next '`user not found`' unless user
+        next '`no events in the past week`' if user.events.empty?
+
+        e = Discordrb::Webhooks::Embed.new(
+          author: { name: 'View Profile', icon_url: Embeds::OSU_ICON },
+          url: user.profile_url,
+          footer: Embeds::VERSION_FOOTER,
+          timestamp: Time.now,
+          color: 0xFF69B4
+        )
+
+        events = user.events.take(10).map do |ev|
+          html = ev.display_html
+          html.gsub!('/b/', "#{Osu::API::BASE_URL}/b/")
+          html.gsub! user.name, "️`#{ev.date.strftime('[%m-%d] %H:%M')}`"
+          Upmark.convert html
+        end
+
+        e.description = events.join "\n\n"
+
+        event.channel.send_embed "`recent events:` **#{user.name}**", e
+      end
+
       command(:ranks,
               description: 'shows you ranks in each game mode',
               usage: 'ccr.ranks username',
